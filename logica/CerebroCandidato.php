883c6b415cd828ec5d4a72a901b4b1950046b7ae
marco <marco@LAPTOP-5DU0FES0>
2017-12-01 19:54:47 -0600

    Se organizaron las carpetas y se genero el scripr para la vista del candidato.

--- logica/CerebroCandidato.php
+++ logica/CerebroCandidato.php
@@ -6,24 +6,26 @@ include '../base/candidatoDBHelper.php';
  *
  * @author marco
  */
-//class CerebroCandidato {
+class CerebroCandidato {
     //put your code here
-    $datos = $_GET['datos'];
-    $method = $_SERVER['REQUEST_METHOD'];
-    /*
-     * De todo lo que envio el cliente busca un llave especifica por cuestiones
-    de seguridad.
-     */
-    $option = basename(filter_input(INPUT_GET, 'option', FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW));
-    
-    /*
-     * 
-     */
-    if($method == "GET" || $option == "candidatosNoAceptados" ){
-        $mHelperCandidato = new candidatoDBHelper();
-        echo $mHelperCandidato->getNoAceptados();
-        
+    function getCandidatosNoAceptados() {
+        $datos = $_GET['datos'];
+        $method = $_SERVER['REQUEST_METHOD'];
+        /*
+         * De todo lo que envio el cliente busca un llave especifica por cuestiones
+        de seguridad.
+         */
+        $option = basename(filter_input(INPUT_GET, 'option', FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW));
+
+        /*
+         * 
+         */
+        if($method == "GET" || $option == "candidatosNoAceptados" ){
+            $mHelperCandidato = new candidatoDBHelper();
+            echo $mHelperCandidato->getNoAceptados();
+
+        }
     }
-//}
+}
 
 ?>
